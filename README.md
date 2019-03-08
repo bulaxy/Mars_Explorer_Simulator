4d5f496b3ad60697fb53cf3c404780b9ab65f830
bulaxy <43806377+bulaxy@users.noreply.github.com>
2019-03-08 11:49:26 +1100

    Preparing Readme, updating simulator and prepare class to run from command line

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
 # Mars_Explorer_Simulator
+
+Mars Explorer Simulator is a simulation of a Mars explorer's movement within a 5 by 5 area. 
+You will be placing the explorer, maneuvering the explorer and asking the explorer to report his/her current location. 
+
+There are three action the explorer can take:
+
+- Landing (Placing) the explorer onto Mars
+- Maneuvering (Moving) the explorer within the safe area in Mars
+- Requesting for rescue (Reporting) from current coordinate
+ 
+ note: explorer will only move vertically or horizontally
+## How do I use the simulator
+The explorer can be land/move within the 5 by 5 area. that is within 0 to 4 of both X and Y dimension. 
+#####Landing
+The explorer can be landed anywhere within the 5 by 5 area, this is done by entering 
+
+    PLACE X,Y 
+
+where X and Y are the place the explorer is landing onto. 
+This command will not give any output unless the input is invalid. 
+
+For example entering `PLACE 1,1` will place the explorer to `(1,1)` and no output will be shown. 
+However, if entered `PLACE 50,50`, error message of `Invalid placement` will be shown.
+
+When a new explorer is landed, the old explorer will be abandoned and previous movement and his location will be lost.
+
+
+#####Maneuvering
+
+The explorer can maneuver within the safe area in mars, this is done by entering 
+
+    MOVE X,Y
+
+where the explorer will move X unit on the X dimension and Y unit to the Y dimension. This commend will output how the explorer is moving. 
+However, if the X or Y are invalid input such as out of the safe area in Mars, the explorer will ignore the command.
+
+For example
+
+    PLACE 0,0
+    MOVE 1,1
+        
+will ouput the explorer landed on 
+
+    M:(0,0) (1,0) (1,1)
+
+as a shown on how to explorer is moved from `(0,0)` to `(1,1)`
+
+However, if an invalid movement is to be entered. For example,
+ 
+    MOVE 5,5
+ is being requested, it will output 
+    
+    Invalid Movement
+#####Requesting for rescue
+The explorer can terminate his exploration by sending explorer's current coordinate. This is done by entering
+
+    REPORT
+    
+where the explorer's current location will be send and exploration will be terminated. It will shows an
+output of 
+
+    P:(X,Y)
+where X and Y are the explorer's current location.
+
+--------------------
+##Test data
+
+A number of test have been conducted to ensure the simulator run smoothly and have expected output
+
+|Test Number| Test Data| Expected outcome | Pass/Fail| Comment|
+|:---------:|:--------:|:----------------:|:--------:|:------:|
+|1|`PLACE 3,3`|(Blank)|P||
+|2|`PLACE -2,-2`|`Invalid placement`|P||
+|3|`PLACE`|`Invalid Input`|P||
+|4|`PLACE 3`|`Invalid Input`|P||
+|5|`PLACE 3,,3`|`Invalid Input`|P||
+|6|`PLACE 3,3MOVE1,1`|`Invalid Input`|P||
+|7|`PLACE 3,3` enter `MOVE 1,1`|`M:(3,3) (4,3) (4,4)` or `M:(3,3) (3,4) (4,4)`|P||
+|8|`PLACE 3,3` enter `MOVE 1,1` enter `REPORT`|`M:(3,3) (4,3) (4,4)` or `M:(3,3) (3,4) (4,4)` followed by `P:(4,4)` and program exit|P||
+|9|`PLACE 3,3` enter `MOVE 5,5`|`Invalid Movement`|P||
+|10|`PlacE 1,2` |(Blank)|P|Simulator not case sensitive|
+|11|`3,3`|`Invalid Input`|P||
+|12|`MOVE 3,3`| `Explorer have not been placed`|P|Move will be ignore if explorer have not been placed|
+|13|`REPORT`|`Explorer have not been placed` and program exit|P||
--- src/MarsExplorer.java
+++ src/MarsExplorer.java
@@ -6,62 +6,67 @@ public class MarsExplorer {
 
         ArrayList<int[]> movement = new ArrayList<int[]>();
         Scanner movementScanner = new Scanner(System.in);
-        String commend = movementScanner.nextLine();
+        String command = movementScanner.nextLine().toUpperCase();
 
-        while(!commend.equals("REPORT")){
-            //TODO: Input Validation
+        while(!command.equals("REPORT")){
+            try {
+                //Identify inputted method and X,Y coordinate input
+                String firstWord = command.substring(0, command.indexOf(" "));
+                int X = Integer.parseInt(command.substring(command.indexOf(" ") + 1, command.indexOf(",")));
+                int Y = Integer.parseInt(command.substring(command.indexOf(",") + 1));
 
-            //Identify inputted method and X,Y coordinate input
-            String firstWord = commend.substring(0, commend.indexOf(" "));
-            int X = Integer.parseInt(commend.substring(commend.indexOf(" ")+1,commend.indexOf(",")));
-            int Y = Integer.parseInt(commend.substring(commend.indexOf(",")+1));
+                if (firstWord.equals("PLACE")) {
+                    //Validating coordinate
+                    if (X >= 0 && Y >= 0 && X <= 4 && Y <= 4) {
+                        //Clearing previous movement and placing explorer location
+                        int[] placedLocation = new int[]{X, Y};
+                        movement.clear();
+                        movement.add(0, placedLocation);
+                    } else {
+                        System.out.println("Invalid placement");
+                    }
+                } else if (firstWord.equals("MOVE")) {
+                    //Check whether explorer have been placed
+                    if (!movement.isEmpty()) {
+                        //Set Current Explorer Location
+                        int[] location = new int[]{movement.get(movement.size() - 1)[0], movement.get(movement.size() - 1)[1]};
 
-            if (firstWord.equals("PLACE")) {
-                //Validating coordinate
-                if(X>=0 && Y>=0 && X<=5 && Y<=5) {
-                    //Clearing previous movement and placing explorer location
-                    int[] placedLocation = new int[]{X, Y};
-                    movement.clear();
-                    movement.add(0,placedLocation);
-                }else{
-                    System.out.println("Invalid placement");
-                }
-            }else if(firstWord.equals("MOVE")){
-                //Check whether explorer have been placed
-                if(!movement.isEmpty()) {
-                    //Set Current Explorer Location
-                    int[] location = new int[]{movement.get(movement.size() - 1)[0], movement.get(movement.size() - 1)[1]};
+                        //Check whether input is valid or off the table
+                        if (location[0] + X <= 4 && location[1] + Y <= 4 && X + Y != 0 && X>=0 && Y>=0) {
 
-                    //Check whether input is valid or off the table
-                    if(location[0]+X<=5 && location[1]+Y<=5 && X+Y!=0) {
-                        //move block by block until reaches inputted number and storing each movement
-                        for (int i = 0; i<X ;i++){
-                            location[0]++;
-                            int[] tempLocation = new int[]{location[0], location[1]};
-                            movement.add(tempLocation);
+                            //move block by block until reaches inputted number and storing each movement
+                            for (int i = 0; i < X; i++) {
+                                location[0]++;
+                                int[] tempLocation = new int[]{location[0], location[1]};
+                                movement.add(tempLocation);
+                            }
+                            for (int i = 0; i < Y; i++) {
+                                location[1]++;
+                                int[] tempLocation = new int[]{location[0], location[1]};
+                                movement.add(tempLocation);
+                            }
+
+                            //Output movement
+                            System.out.print("M: ");
+                            for (int[] tempMovement : movement) {
+                                System.out.print("(" + tempMovement[0] + "," + tempMovement[1] + ") ");
+                            }
+                            System.out.println();
+
+                        } else {
+                            System.out.println("Invalid movement");
                         }
-                        for (int i = 0; i<Y ;i++){
-                            location[1]++;
-                            int[] tempLocation = new int[] {location[0], location[1]};
-                            movement.add(tempLocation);
-                        }
-                        //TODO: Negative movement?
-
-                        //Output movement
-                        System.out.print("M: ");
-                        for (int[] tempMovement : movement) {
-                            System.out.print("(" + tempMovement[0] + "," + tempMovement[1] + ") ");
-                        }
-                        System.out.println();
-                    }else{
-                        System.out.println("Invalid movement");
+                    } else {
+                        System.out.println("Explorer have not been placed");
                     }
                 }else{
-                    System.out.println("Explorer have not been placed");
+                    System.out.println("Invalid command");
                 }
+            } catch(Exception e){
+                System.out.println("Invalid Input");    //Any error will leads to Invalid Input
             }
             //Next Input
-            commend = movementScanner.nextLine();
+            command = movementScanner.nextLine().toUpperCase();
         }
         //If input Report, output latest location
         if(!movement.isEmpty()) {
